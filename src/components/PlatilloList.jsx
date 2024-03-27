a9d8411fd01ffb9720c40477eb21958bda005bde
ricardocastellr <rcastell@ucol.mx>
2024-03-27 17:13:02 -0600

    Remove index from PlatilloList

--- src/components/PlatilloList.jsx
+++ src/components/PlatilloList.jsx
@@ -3,9 +3,9 @@ import { Platillo } from './Platillo'
 export function PlatilloList({ dishes, handleDelete, handleUpdate }) {
   return (
     <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-      {dishes.map((dish, index) => (
-        <div key={index}>
-          <Platillo dish={dish} index={index} handleDelete={handleDelete} handleUpdate={handleUpdate} />
+      {dishes.map((dish) => (
+        <div>
+          <Platillo dish={dish} handleDelete={handleDelete} handleUpdate={handleUpdate} />
         </div>
       ))}
     </div>
