dc6cc9f278feb65be9a08b6d67e8c9541a21b5b2
CecilLM55 <clopez16@ucol.mx>
2024-04-09 12:27:05 -0600

    added fonts to tailwind and changes in platilloform

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sriracha': ['Sriracha', 'cursive'],
+        'tauri': ['Tauri', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
