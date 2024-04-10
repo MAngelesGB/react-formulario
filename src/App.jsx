cb7ffb3274f459228c06b09d49e81cec4fdcd07b
CarmenSRM <cramos15@ucol.mx>
2024-04-10 01:02:10 -0600

    Cards an images

--- src/App.jsx
+++ src/App.jsx
@@ -145,8 +145,8 @@ function App() {
 
   return (
     <>
-    <div className="bg-[#B9CDDA]">
-      <h1 className="font-sriracha text-[#6B7D7D] text-[48px] text-center">Platillos</h1>
+    <div className="bg-[#B9CDDA] h-[110px]">
+      <h1 className="font-sriracha text-[#6B7D7D] text-[48px] text-center pt-4">Platillos</h1>
     </div>
       {isFormVisible && (
 
@@ -162,17 +162,17 @@ function App() {
         </div>
       )}
 
-      <div className='flex flex-row justify-evenly items-center '>
+      <div className='flex flex-row justify-evenly items-center mt-6'>
         <div className="my-4 font-tauri ml-4">
           <input
             type="text"
             placeholder="Buscar platillo"
             value={filter}
             onChange={handleFilterChange}
-            className="border border-gray-300 rounded px-4 py-2 w-[1200px] rounded-[6px] "
+            className="border border-gray-300 rounded px-4 py-2 w-[1500px] rounded-[15px] "
           />
-          <select  className=" bg-[#8EAF9D] ml-3 rounded-[6px] hover:bg-[#8EAF9D] text-white h-[40px]"onChange={(e) => sortDishes(e.target.value)}>
-            <option value="">  </option>
+          <select  className=" bg-[#8EAF9D] text-[#FFFFFF]  text-center ml-3 rounded-[10px] hover:bg-[#FFFFFF] hover:text-[#6B7D7D]  w-[150px]  h-[40px]"onChange={(e) => sortDishes(e.target.value)}>
+            <option value=""> Ordenado por </option>
             <option value="nombre">Nombre</option>
             <option value="personas">Personas</option>
           </select>
@@ -180,7 +180,7 @@ function App() {
 
         <a
           onClick={() => setIsFormVisible(true)}
-          className={`my-4 ${isFormVisible ? 'hidden' : 'block'} py-2 px-4 rounded hover:scale-105 cursor-pointer`}
+          className={` ${isFormVisible ? 'hidden' : 'block'} rounded hover:scale-105 cursor-pointer`}
         >
           <svg width="50" height="50" viewBox="0 0 24 24"><path fill="#8EAF9D" d="M11 17h2v-4h4v-2h-4V7h-2v4H7v2h4zm1 5q-2.075 0-3.9-.788t-3.175-2.137q-1.35-1.35-2.137-3.175T2 12q0-2.075.788-3.9t2.137-3.175q1.35-1.35 3.175-2.137T12 2q2.075 0 3.9.788t3.175 2.137q1.35 1.35 2.138 3.175T22 12q0 2.075-.788 3.9t-2.137 3.175q-1.35 1.35-3.175 2.138T12 22" /></svg>
         </a>
@@ -189,7 +189,7 @@ function App() {
       <div className="my-4">
         {
           filteredDishes.length === 0 && (
-            <div className="text-center text-gray-500">
+            <div className="text-center text-[30px] font-tauri text-[#6B7D7D]">
               No hay platillos
             </div>
           )
--- src/components/Platillo.jsx
+++ src/components/Platillo.jsx
@@ -1,19 +1,19 @@
 export function Platillo({ dish, handleDelete, handleUpdate }) {
   return (
-    <div className="max-w-[400px] max-h-[600px] border-gray-300 rounded-lg p-4 bg-white m-10">
-      <div className="flex justify-center items-center">
-        <img src={dish.imagen} alt={dish.nombre} className="w-80 h-60 object-cover" />
+    <div className="max-w-[800px] max-h-[600px] border-gray-300 rounded-lg bg-white m-9 rounded-[25px] ">
+      <div className="flex justify-center items-center ">
+        <img src={dish.imagen} alt={dish.nombre} className="w-80 h-60 object-cover rounded-[15px]" />
       </div>
       <div className="my-4 flex flex-col items-center mb-4 mt-4">
         <h2 className="text-lg font-sriracha text-[#6B7D7D] text-[32px]">{dish.nombre}</h2>
         <p className="font-sriracha text-[#A6D8D4] text-[22px]">{dish.descripcion}</p>
       </div>
-      <ul className="font-sriracha text-[#D7DAE5] text-[18px]">
+      <ul className="font-sriracha text-[#D7DAE5] text-[18px] p-4">
         <li>Acompañantes: {dish.acompaniantes.join(', ')}</li>
         <li>Picor: {dish.picor}</li>
         <li>Personas: {dish.personas}</li>
       </ul>
-      <div className="flex justify-center items-center mt-4">
+      <div className="flex justify-center items-center mt-4 p-4">
         <button onClick={() => handleUpdate(dish.id)} className=" max-w-[150px] text-[18px] bg-[#A6D8D4] hover:bg-[#8EAF9D] text-white font-sriracha py-2 px-5  rounded-[8px] hover:scale-105">Actualizar</button>
         <button onClick={() => handleDelete(dish.id)} className=" max-w-[150px] text-[18px] bg-[#6B7D7D] hover:bg-[#8EAF9D] text-white font-sriracha py-2 px-5 ml-3 rounded-[8px] hover:scale-105">Eliminar</button>
       </div>
